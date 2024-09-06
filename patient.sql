7ff89e88d30cde396b2441003bff4ad6933430ed
Hamrithnahil <143583005+hamrith13@users.noreply.github.com>
2024-09-06 15:32:39 +0530

    Add files via upload

---
+++ patient.sql
@@ -0,0 +1,20 @@
+
+
+USE hospital_db;
+
+-- Create patients table
+CREATE TABLE patients (
+    patient_id VARCHAR(10) PRIMARY KEY,
+    username VARCHAR(100) NOT NULL,
+    dob DATE NOT NULL
+);
+
+-- Create documents table
+CREATE TABLE documents (
+    document_id INT AUTO_INCREMENT PRIMARY KEY,
+    patient_id VARCHAR(10),
+    document_name VARCHAR(255) NOT NULL,
+    file_path VARCHAR(255) NOT NULL,
+    upload_date DATETIME NOT NULL,
+    FOREIGN KEY (patient_id) REFERENCES patients(patient_id)
+);
